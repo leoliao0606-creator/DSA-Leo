af1e2eea7886f2e3ba151e78ea6824193e3c6258
leo-debian <leoliao0606@gmail.com>
2026-09-17 13:16:03 -0400

    I didnt save this file when I last commit, so some comments were missing

--- Assignment2/Exercises.hpp
+++ Assignment2/Exercises.hpp
@@ -20,20 +20,27 @@ template <typename T>
 Stack<T> copyStack(Stack<T>& stack) {
     Queue<T> queue;
     Stack<T> result;
-    while(!stack.isEmpty()) {
+    while(!stack.isEmpty()) { // copty from original stack to queue (order reversed)
         auto value = stack.pop();
         queue.enqueue(value.value());
     }
-    while(!queue.isEmpty()) {
+    while(!queue.isEmpty()) { // copy from queue to the original stack (order unchanged)
         auto value = queue.dequeue();
         stack.push(value.value());
     }
-    while(!stack.isEmpty()) {
+    while(!stack.isEmpty()) { // copy from original stack to both queue (order reversed) and new stack (order reversed)
         auto value = stack.pop();
         result.push(value.value());
         queue.enqueue(value.value());
     }
-    while(!queue.isEmpty()) {
+    /*
+    This reason why I have an extra loop here is that we didn't implemented the deep copy function for stack,
+    which is an unique feature in C++, while passing parameters by value requires deep copy to avoid conflits, 
+    so I can only set the paramenter of the current function to be pass by reference. This means that if I 
+    make any change to the parameter pass in, the same variable where it's passed in a function call will also
+    be modified. Hence I have to restore the stack to it's original state before returning. 
+    */ 
+    while(!queue.isEmpty()) { // copy from queue to original stack (order unchanged)
         auto value = queue.dequeue();
         stack.push(value.value());
     }
